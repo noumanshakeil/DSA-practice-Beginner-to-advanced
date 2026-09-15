be6754c3642029425a4f723f2dc352bf1a83f2d8
Muhammad Nouman Shakeel <nouman.shakeel555@gmail.com>
2026-09-15 17:43:12 +0500

    Create 2-sum.cpp

---
+++ 2-sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cout << "Enter a and b numbers:" << endl;
+    cin >> a >> b;
+    int sum = a + b;
+    cout << "sum of a and b is: " << sum;
+    return 0;
+}
