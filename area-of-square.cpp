2fb40aaca2f8b451938676fca034487236096e79
Muhammad Nouman Shakeel <nouman.shakeel555@gmail.com>
2026-09-15 17:44:40 +0500

    Create area-of-square.cpp

---
+++ area-of-square.cpp
@@ -0,0 +1,12 @@
+#inlcude <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    int a, b;
+    cout << "Enter a and b numbers:" << endl;
+    cin >> a >> b;
+    int area = a * b;
+    cout << "area of a and b is: " << area;
+    return 0;
+}
